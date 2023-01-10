4dfe5e358014baa9d2834eb4809cb50cf0f2b87c
Julia Huessy <jhuessy@hotmail.com>
2023-01-10 10:00:31 -0500

    start of project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./header/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <h2>Hello from App</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+      <header className="container-fluid text-center bg-success text-white padding-5 mb-3">
+          <h1>Yelpington</h1>
+      </header>
+  )
+}
+
+export default Header
